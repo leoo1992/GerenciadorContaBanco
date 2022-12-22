e83d9da94fbe5948c0974ad80032e70333c3919e
Leo <santos-contato@hotmail.com.br>
2022-12-22 17:19:02 -0300

    exercicio da DIO resolvido

--- ContaBanco/src/ContaNoTerminal.java
+++ ContaBanco/src/ContaNoTerminal.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
+// desafio de codigo da DIO
+//desafio disponivel em : https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/sintaxe
+
 public class ContaNoTerminal {
-    
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("    Bem vinvo ao BANCO X    ");
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite o numero da sua conta: ");
+        int numeroConta = ler.nextInt();
+        System.out.println("Digite o numero da sua agencia: ");
+        String agencia = ler.next();
+        System.out.println("Digite o nome do cliente: ");
+        String nomeCliente = ler.next();
+        System.out.println("Digite o saldo da conta: ");
+        double saldo = ler.nextDouble();
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
